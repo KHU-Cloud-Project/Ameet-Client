2c627cc629c0dc030debe3e0b9c6bb54c41883e6
cherrie-k <cherriekim925@gmail.com>
2024-10-28 04:18:29 +0900

    feat: 예시 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,28 @@ import { Route, Routes } from 'react-router';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import { BrowserRouter } from 'react-router-dom';
+import styled from '@emotion/styled';
 
-// 스타일 정의
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100vw;
+`;
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <AppContainer>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </AppContainer>
     </ThemeProvider>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import { Spacer } from '../components/common/Spacer';
 const Dashboard = () => {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const handleBtnClick = () => {
     navigate('/login');
   };
 
@@ -12,7 +12,7 @@ const Dashboard = () => {
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h2>Dashboard Page</h2>
       <Spacer height={30} />
-      <button onClick={handleLogin}>로그인 페이지로 이동</button>
+      <button onClick={handleBtnClick}>로그인 페이지로 이동</button>
     </div>
   );
 };
