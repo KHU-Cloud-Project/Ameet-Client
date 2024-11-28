20c5cb571753a97a3dccc729e3c48506feac46f1
cherrie-k <cherriekim925@gmail.com>
2024-11-28 22:00:14 +0900

    feat: 회의록 업로드 모달에 date picker 구현

--- src/components/common/logBoard/UploadModal.tsx
+++ src/components/common/logBoard/UploadModal.tsx
@@ -7,6 +7,8 @@ import ModalContainer from '../../common/modal/ModalContainer';
 import InputLabel from '../InputLabel';
 import { AiOutlineClose } from 'react-icons/ai';
 import uploadIcon from '/src/assets/icons/logs/upload.png';
+import DatePicker from './DatePicker';
+import CustomBtn from '../CustomBtn';
 
 const TitleArea = styled.div`
   display: inline-block;
@@ -22,6 +24,7 @@ const Content = styled.div`
   justify-content: space-between;
   width: 100%;
   gap: 20px;
+  margin-bottom: 38px;
 `;
 
 const LeftColumn = styled.div`
@@ -99,22 +102,11 @@ const RightColumn = styled.div`
   flex-direction: column;
 `;
 
-const DatePicker = styled.div`
-  width: 100%;
-  height: 100px;
-  background-color: ${(props) => props.theme.colors.pastelPink};
-  border-radius: ${(props) => props.theme.borderRadius.small};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: ${(props) => props.theme.typography.fontSize.default};
-  color: ${(props) => props.theme.colors.textGray};
-`;
-
 const UploadModal = ({ onClose }: { onClose: () => void }) => {
   const [title, setTitle] = useState('');
   const [members, setMembers] = useState('');
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length > 1) {
@@ -138,7 +130,7 @@ const UploadModal = ({ onClose }: { onClose: () => void }) => {
       'audio/wav': [],
       'audio/aac': [],
     },
-    maxSize: 100 * 1024 * 1024, // 10 MB
+    maxSize: 100 * 1024 * 1024, // 100 MB
     onDropRejected: (fileRejections) => {
       const message = fileRejections[0]?.errors[0]?.message || 'File rejected';
       alert(message);
@@ -192,9 +184,21 @@ const UploadModal = ({ onClose }: { onClose: () => void }) => {
               placeholder="Add list of members"
               onChange={(e) => setMembers(e.target.value)}
             ></InputLabel>
-            <DatePicker>Date Picker UI</DatePicker>
+            <DatePicker
+              label="Date and Time"
+              selectedDate={selectedDate}
+              onChange={setSelectedDate}
+            />
           </RightColumn>
         </Content>
+        <CustomBtn
+          text="Upload"
+          padding="14px 28px"
+          onClick={() => {
+            console.log('upload meeting audio');
+          }}
+          disabled={!title || !members || !selectedDate}
+        />
       </ModalContainer>
     </ModalOverlay>
   );
