2819da7a2dc24357a11e98eba8536e6d039c1a53
cherrie-k <cherriekim925@gmail.com>
2024-12-02 05:16:42 +0900

    design: 팀 스페이스 리스트 디자인 수정

--- src/components/dashboard/leftPanel/SpaceItem.tsx
+++ src/components/dashboard/leftPanel/SpaceItem.tsx
@@ -10,11 +10,16 @@ type SpaceItemProps = {
 
 const Item = styled.div`
   font-size: ${(props) => props.theme.typography.fontSize.medium};
-  color: ${(props) => props.theme.colors.textBlack};
+  color: ${(props) => props.theme.colors.textDarkGray};
   display: flex;
   align-items: center;
   gap: 10px;
+  padding: 8px 18px;
   cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.darkWhite};
+  }
 `;
 
 function SpaceItem({ team }: SpaceItemProps) {
