9ad516db1a04cb58cac170aca8ae4f524af3bd59
cherrie-k <cherriekim925@gmail.com>
2024-12-04 04:03:44 +0900

    feat: api 반환 데이터 반영, 날짜 파싱 문제 해결

--- src/models/Log.ts
+++ src/models/Log.ts
@@ -2,10 +2,14 @@ import { UserForTeam } from '../recoil/atoms/userAtom';
 
 export type Log = {
   meetingId: number;
+  noteId?: number;
   title: string;
-  date: string;
-  duration: number;
+  summary?: string | null;
+  script?: string | null;
+  presignedUrl?: string | null;
+  createdAt?: string | null;
+  startedAt?: string | null;
+  duration: string | null;
   participants?: UserForTeam[] | null;
-  aiSummary?: string | null;
-  originalContent?: string | null;
+  members?: string | null;
 };
